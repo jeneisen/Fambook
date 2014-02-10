bf7fb39823352f823c06c8d2aaa3b35562c18813
Jen E <jeneisenberg5@gmail.com>
2014-02-10 12:09:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fambook::Application.config.secret_token = 'd9b36dae2af0ab934ba93b243fa24fcce5c2f400cdc3deb222ec0b06cdfc25d2f3df031b2adb05707c78627eb0e98100b9db7f6c4444125a1b371607df80cbdf'
